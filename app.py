6da5b7aaf10e370501e1c4c174d084aa8d606cc4
navitacion <masataka.i.navi@gmail.com>
2020-11-21 19:06:54 +0900

    refactering

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 import torch
 
 from src.models.cycle_gan import CycleGAN_Unet_Generator
-from src.utils.utils import ImageTransform
+from src.utils.utils import ImageTransform, get_binary_file_downloader_html
 
 @st.cache
 def model_builder(style='monet'):
@@ -24,21 +24,28 @@ def model_builder(style='monet'):
 def app():
     # Title
     st.title('AI Painter')
+    st.markdown('Produce by [navi](https://twitter.com/navitacion)')
     st.markdown('---')
     st.markdown('This app is a demo of CycleGAN')
+    st.markdown("v1: You can use the painter's style written below")
+    st.markdown("- Monet\n- Van Gogh")
     st.markdown('---')
 
-    style = st.selectbox('Select GAN', ('monet', 'vangogh'))
+    style = st.sidebar.selectbox('Select Style', ('monet', 'vangogh'))
 
     net = model_builder(style)
-    uploaded_file = st.file_uploader("Choose a file")
+    uploaded_file = st.sidebar.file_uploader("Choose Your Image")
 
     transform = ImageTransform()
 
-    if uploaded_file is not None:
+    if uploaded_file is None:
+        st.info("Please upload your image from sidebar list.\n\n After uploading, you can get the painter's style image soon.")
+    else:
         bytes_data = uploaded_file.read()
         image = Image.open(BytesIO(bytes_data))
         w, h = image.size
+
+        st.subheader('Input Image')
         st.image(image, use_column_width=True)
 
         inp = transform(image, phase='test')
@@ -53,10 +60,15 @@ def app():
         out = out.permute(1, 2, 0).detach().numpy().astype(np.uint8)
 
         out = Image.fromarray(out)
+        out.save('output.jpg')
         out = out.resize((w, h))
 
+        st.markdown('---')
+        st.subheader('Output Image')
         st.image(out, use_column_width=True)
 
+        tmp_download_link = get_binary_file_downloader_html('output.jpg', 'Picture')
+        st.markdown(tmp_download_link, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     app()
